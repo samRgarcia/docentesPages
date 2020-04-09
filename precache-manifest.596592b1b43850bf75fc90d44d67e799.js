53e6ea0f5ef9873ca890ccbb50cd72791366e770
samRgarcia <samuelrivas18@gmail.com>
2020-04-09 23:35:28 +0200

    Updates

---
+++ precache-manifest.596592b1b43850bf75fc90d44d67e799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ade33351de26612d4c5fef175e2e0dae",
+    "url": "/docentesPeges/index.html"
+  },
+  {
+    "revision": "49e175ef6cf1e219a77f",
+    "url": "/docentesPeges/static/css/main.e880cbef.chunk.css"
+  },
+  {
+    "revision": "c9865edbb728db962c2e",
+    "url": "/docentesPeges/static/js/2.fd554c10.chunk.js"
+  },
+  {
+    "revision": "9a025d5c0f91beed6140601873f69a5c",
+    "url": "/docentesPeges/static/js/2.fd554c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e175ef6cf1e219a77f",
+    "url": "/docentesPeges/static/js/main.e3cfc529.chunk.js"
+  },
+  {
+    "revision": "e2e81eb4b00cbcc2d0c7",
+    "url": "/docentesPeges/static/js/runtime-main.94fdea20.js"
+  }
+]);
